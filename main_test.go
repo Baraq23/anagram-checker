bf37af7cf201955e008e4dcdb91653a6c45918ac
barraotieno <barrackotieno160@gmail.com>
2024-07-13 09:21:11 +0300

    modified testfiles. function main test added as comment

--- main_test.go
+++ main_test.go
@@ -49,3 +49,29 @@ func TestValidateStr(t *testing.T) {
 		}
 	}
 }
+
+
+// TestFibonacciNum tests the FibonacciNum function
+// func TestFibonacciNum(t *testing.T) {
+// 	tests := []struct {
+// 		input    int
+// 		expected int
+// 	}{
+// 		{0, 0},
+// 		{1, 1},
+// 		{2, 1},
+// 		{3, 2},
+// 		{4, 3},
+// 		{5, 5},
+// 		{10, 55},
+// 		{20, 6765},
+// 		{30, 832040},
+// 	}
+
+// 	for _, test := range tests {
+// 		result := FibonacciNum(test.input)
+// 		if result != test.expected {
+// 			t.Errorf("FibonacciNum(%d) = %d; expected %d", test.input, result, test.expected)
+// 		}
+// 	}
+// }
